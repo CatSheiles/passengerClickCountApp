becd60afed8ae49a6c3fdcce9a77949d288f1928
CatSheiles <cathysheiles@gmail.com>
2021-05-23 11:05:24 +1000

    Initial

---
+++ index.js
@@ -0,0 +1,28 @@
+//document.getElementById("count-el").innerText = 5;
+
+// store the count
+//let count = 0;
+
+//console.log(count);
+
+//ititialize the count as 0
+//listen for clicks on incrementBtn
+//increm count variable when btn clicked on
+//change count-el in html to reflect new count
+
+//function increment() {
+//    console.log("the button was clicked")
+//}
+
+let laps = 0;
+
+function complaps() {
+    laps = laps + 1
+ 
+}
+
+complaps();
+complaps();
+complaps();
+
+console.log(laps);
